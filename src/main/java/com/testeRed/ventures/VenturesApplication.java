6b40d3817422debb8ee7df4ebd14b65ae31602c7
miguelpan <miguel_tobi@hotmail.com>
2024-05-29 19:02:28 -0300

    Criação do projeto versão inicial

---
+++ src/main/java/com/testeRed/ventures/VenturesApplication.java
@@ -0,0 +1,13 @@
+package com.testeRed.ventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testeRed/ventures/VenturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeRed.ventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
